94e287c7df536e1e62291a59bd03d6ef514d3364
Ming-1103 <mingjia736@gmail.com>
2026-07-17 16:09:25 +0800

    netbean structure

---
+++ src/resort/management/Main.java
@@ -0,0 +1,7 @@
+package resort.management;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Resort Management System Initialized successfully!");
+    }
+}
